d0b5618a223f5cb477767f7c74acf9570ed1601e
Thejwin <thejwin123@gmail.com>
2025-09-14 22:52:43 +0530

    Game functionality with attempt tracking

--- vs-ai.js
+++ vs-ai.js
@@ -1,8 +1,12 @@
-let userInput = 'save'; 
-
+let userInput = 'shoot'; 
 let playable = false;
 let bottomPos = -200;
 let leftPos = 50;
+let playerScore = 0;
+let opponentScore = 0;
+let currentRound = 0;
+let totalRound = 5
+let gameStarted = false;
 
 let invertedKeeper = false;
 
@@ -11,6 +15,10 @@ const btn_middle = document.getElementById("btn-middle");
 const btn_right = document.getElementById("btn-right");
 const infoText = document.getElementById("info-texts");
 const infoTextH2 = document.getElementById("infoTextH2");
+const scorePlayer = document.getElementById("player-score");
+const scoreOpponent = document.getElementById("opponent-score");
+const playerAttempts = document.querySelectorAll(".score-box-player");
+const opponentAttempts = document.querySelectorAll(".score-box-opponent");
 
 infoTextH2.addEventListener('click',() => initGame());
 btn_left.addEventListener('click',() => shootOrSave("left"));
@@ -20,9 +28,9 @@ btn_right.addEventListener('click',() => shootOrSave("right"));
 const keeper = document.getElementById("keeper");
 const ball = document.getElementById("ball");
 
-function  initGame() {
-  playable = true;
-  infoTextH2.innerText = "Shoot";
+function chooseRandom() {
+  let options = ['left', 'middle', 'right'];
+  return options[Math.floor(Math.random() * 3)];
 }
 
 function shoot(direction) {
@@ -79,25 +87,111 @@ function resetSprites(){
   leftPos = 50;
 }
 
+function updateScores() {
+  scoreOpponent.textContent = opponentScore;
+  scorePlayer.textContent = playerScore;
+}
+
 function shootOrSave(direction) {
   if (playable){
     if (userInput === 'shoot') {
+      infoTextH2.innerText = "SHOOT!";
+      let opponentChoice = chooseRandom();
       shoot(direction);
-      //playable = false;
-    }
-    else{ 
-      save(direction);
-      shoot(direction);
+      save(opponentChoice);
       playable = false;
-      infoTextH2.innerText = "It's a save :(";
+      if (direction === opponentChoice) {
+        infoTextH2.innerText = "It's a save";
+        playerAttempts.forEach((box) => {
+          if (box.id === String(currentRound)) {
+            box.style.backgroundColor= "rgba(255, 0, 0, 1)";
+          }
+        });
+      }
+      else {
+        infoTextH2.innerText = "It's a goal";
+        playerScore++;
+        playerAttempts.forEach((box) => {
+          if (box.id === String(currentRound)) {
+            box.style.backgroundColor= "rgb(127, 255, 0)";
+          }
+        });
+      }
+      userInput = 'save';
     }
+
+    else{ 
+      infoTextH2.innerText = "SAVE!";
+      let opponentChoice = chooseRandom();
+      save(direction);
+      shoot(opponentChoice);
+      playable = false;
+      if (direction === opponentChoice) {
+        infoTextH2.innerText = "It's a save";
+        opponentAttempts.forEach((box) => {
+          if (box.id === String(currentRound)) {
+            box.style.backgroundColor= "rgba(255, 0, 0, 1)";
+          }
+        });
+      }
+      else {
+        infoTextH2.innerText = "It's a goal";
+        opponentScore++;
+        opponentAttempts.forEach((box) => {
+          if (box.id === String(currentRound)) {
+            box.style.backgroundColor= "rgb(127, 255, 0)";
+          }
+        });
+      }
+      userInput = 'shoot';
+      currentRound++;
+      if (currentRound === 5) {
+        updateScores();
+        endGame();
+        return;
+      }
+    }
+
 
     setTimeout(() => {
       resetSprites();
-      infoTextH2.innerText = "Shoot";
+      updateScores();
       setTimeout(() => {
         playable = true;
       }, 1000);
+      infoTextH2.innerText = `${userInput}!`.toUpperCase();
     }, 1000);
   }
+}
+
+function resetAttempts() {
+  opponentAttempts.forEach((box) => {
+  box.style.backgroundColor= "aliceblue";
+  });
+  playerAttempts.forEach((box) => {
+  box.style.backgroundColor= "aliceblue";
+  });
+}
+
+function endGame() {
+  // code to announce winner and reset things
+
+  infoTextH2.innerText = "Restart";
+  playable = false
+  gameStarted = false;
+}
+
+function  initGame() {
+  if (!gameStarted) {
+  playerScore = 0;
+  opponentScore = 0;
+  currentRound = 0;
+  resetAttempts();
+  resetSprites();
+  updateScores();
+  playable = true;
+  infoTextH2.innerText = "SHOOT!";
+  gameStarted = true;
+  }
+
 }
