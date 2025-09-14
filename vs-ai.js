bc57757631f6a6e133e4875b6f11feaf79fd858e
Thejwin <thejwin123@gmail.com>
2025-09-14 13:26:02 +0530

    shooting and saving mechanism, text area

--- vs-ai.js
+++ vs-ai.js
@@ -1,32 +1,101 @@
-/* function shoot(direction) {
-  const ball = document.getElementById("ball");
-  ball.className = ""; // reset
-  void ball.offsetWidth; // force reflow
-  ball.classList.add("shoot-" + direction);
-} */
+let userInput = 'save'; 
+
+let playable = false;
+let bottomPos = -200;
+let leftPos = 50;
+
+let invertedKeeper = false;
 
 const btn_left = document.getElementById("btn-left");
 const btn_middle = document.getElementById("btn-middle");
 const btn_right = document.getElementById("btn-right");
+const infoText = document.getElementById("info-texts");
+const infoTextH2 = document.getElementById("infoTextH2");
 
-btn_left.addEventListener('click',() => shoot("left"));
-btn_middle.addEventListener('click',() => shoot("middle"));
-btn_right.addEventListener('click',() => shoot("right"));
+infoTextH2.addEventListener('click',() => initGame());
+btn_left.addEventListener('click',() => shootOrSave("left"));
+btn_middle.addEventListener('click',() => shootOrSave("middle"));
+btn_right.addEventListener('click',() => shootOrSave("right"));
 
-let bottomPos = -200;
-let leftPos = 50;
+const keeper = document.getElementById("keeper");
+const ball = document.getElementById("ball");
+
+function  initGame() {
+  playable = true;
+  infoTextH2.innerText = "Shoot";
+}
+
 function shoot(direction) {
-  bottomPos += 140;  // move downward
+  bottomPos += 140;  // move up
   if (direction === 'left') {
     leftPos -= 35; // move left
   } 
   else if (direction === 'right') {
     leftPos += 40; // move right
   }
- 
-  const ball = document.getElementById("ball");
+
   ball.style.bottom = bottomPos + "px";
   ball.style.left = leftPos + "%";
-  ball.style.transform = "translateX(-50%) scale(0.25)";
+  ball.style.transform = "translateX(-50%) scale(0.2)";
 }
 
+
+function save(direction) {
+  
+  if (direction === 'middle') {
+    keeper.src = "./img/characters/gk3.png";
+  }
+  else if (direction === 'right' ){
+    keeper.src = "./img/characters/gk2.png";
+    keeper.style.left = "40%";
+    keeper.style.transform = "scale(0.8)";
+    keeper.style.bottom = "-30px";
+  }
+  else {
+    keeper.src = "./img/characters/gk2.png";
+    keeper.style.left = "-20%";
+    keeper.style.transform = "scale(0.8) scaleX(-1)";
+    invertedKeeper = true;
+    keeper.style.bottom = "-30px";
+  }
+}
+
+function resetSprites(){
+  // reset keeper 
+  keeper.src = "./img/characters/gk0.png";
+  keeper.style.left = "50%";
+  keeper.style.transform = "translateX(-50%) scale(1)";
+  if (invertedKeeper) {
+    keeper.style.transform = "translateX(-50%) scale(1) scaleX(-1)";
+    invertedKeeper = false;
+  }
+  keeper.style.bottom = "-10px";
+
+  // reset ball
+  ball.style.bottom = "-200px";
+  ball.style.left = "50%";
+  ball.style.transform = "translateX(-40%) scale(0.8)"
+  bottomPos = -200;
+  leftPos = 50;
+}
+
+function shootOrSave(direction) {
+  if (playable){
+    if (userInput === 'shoot') {
+      shoot(direction);
+      //playable = false;
+    }
+    else{ 
+      save(direction);
+      shoot(direction);
+      infoTextH2.innerText = "It's a save :(";
+      playable = false;
+    }
+
+    setTimeout(() => {
+      resetSprites();
+      playable = true;
+      infoTextH2.innerText = "Shoot";
+    }, 2000);
+  }
+}
